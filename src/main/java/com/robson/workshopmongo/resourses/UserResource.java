8ea814c0ef91a2e4bf77e317cf114163ac6227d4
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-02-01 14:45:18 -0300

    Operacao de instanciacao de base de dados

--- src/main/java/com/robson/workshopmongo/resourses/UserResource.java
+++ src/main/java/com/robson/workshopmongo/resourses/UserResource.java
@@ -1,6 +1,7 @@
 package com.robson.workshopmongo.resourses;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robson.workshopmongo.domain.User;
+import com.robson.workshopmongo.dto.UserDTO;
 import com.robson.workshopmongo.services.UserService;
 
 @RestController
@@ -18,15 +20,22 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@RequestMapping(method=RequestMethod.GET)
+	
+
+	/*@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		/*User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));*/
 		
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list); 
+	}*/
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll(){
+		
+		List<User> list = service.findAll();
+		//Expressao Lambda para converter a lista de User em lista de UserDTO (para cada x que é usuário, ele passa para UserDTO como argumento.
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto); 
 	}
+
+
 }
