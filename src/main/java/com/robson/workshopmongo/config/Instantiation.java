a3914a073a1ee90af3155410c7ec8b6244d742dc
RobsondCCarneiro <robinho.cenep@gmail.com>
2021-02-03 14:51:19 -0300

    Acrescentando comentário aos posts

--- src/main/java/com/robson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/robson/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.robson.workshopmongo.domain.Post;
 import com.robson.workshopmongo.domain.User;
 import com.robson.workshopmongo.dto.AuthorDTO;
+import com.robson.workshopmongo.dto.CommentDTO;
 import com.robson.workshopmongo.repository.PostRepository;
 import com.robson.workshopmongo.repository.UserRepository;
 
@@ -45,6 +46,14 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		//Fazendo as associações entre os comentários c1 e c2 com o post1
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		
+		post2.getComments().addAll(Arrays.asList(c3));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		//Aqui é para referenciar os posts
